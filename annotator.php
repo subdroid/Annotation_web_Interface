b3d00cb737779b2c13bcff9b619849ab6e5c8928
Mukesh Jangir <mukeshjangir29@gmail.com>
2020-12-27 20:06:45 +0530

    New working interface

--- annotator.php
+++ annotator.php
@@ -1,9 +1,14 @@
 <?php
-session_start()
+session_start();
+$imagePath = "DIDEC/images";
+$captionsPath = "DIDEC/captions";
 ?>
+
+<?php include 'utils.php' ?>
 
 <!doctype html>
 <html lang="en">
+
 <head>
     <title>Neurem-3 dataset annotator</title>
     <!-- Required meta tags -->
@@ -18,6 +23,41 @@ session_start()
 
     <!-- Material Dashboard CSS -->
     <link rel="stylesheet" href="assets/css/material-dashboard.css">
+
+    <script>
+        var filenames = <?php echo json_encode(getImagesList($imagePath)) ?>;
+        var captions = <?php echo json_encode(getImageCaptions($captionsPath)) ?>;
+        var index = 0;
+        var clickCount = 0;
+
+        function loadCaptionsAndImages() {
+            clickCount++;
+            if (clickCount == 2) {
+                document.getElementById("captionCardText").innerText = captions[index];
+                index++;
+                clickCount = 0;
+                document.getElementById("captionWithImage").style.display = 'none';
+                document.getElementById("captionOnly").style.display = 'block';
+
+                document.getElementById("captionCardText").innerText = captions[index];
+                document.getElementById('captionWithImage').style.display = 'none';
+                document.getElementById('captionImageCardText').innerText = captions[index];
+                document.getElementById('Image').src = 'DIDEC/images/' + filenames[index] + '.jpg'
+            }
+            if (clickCount == 1) {
+                document.getElementById("captionOnly").style.display = 'none';
+                document.getElementById("captionWithImage").style.display = 'block';
+            }
+        }
+
+        window.onload = function() {
+            document.getElementById("captionCardText").innerText = captions[index];
+            document.getElementById('captionWithImage').style.display = 'none';
+            document.getElementById('captionImageCardText').innerText = captions[index];
+            document.getElementById('Image').src = 'DIDEC/images/' + filenames[index] + '.jpg'
+        };
+    </script>
+
 </head>
 
 <body>
@@ -45,30 +85,44 @@ session_start()
                     <div class="card-body">
                         <div class="row">
                             <!-- Last annotated image -->
-                            <div class="col-md-6">
-                                <div class="card">
+                            <div class="col-md-12" id="captionOnly">
+                                <div class="card" style="width: 60%; margin-left:auto; margin-right:auto;">
                                     <div class="card-header card-header-text card-header-primary">
                                         <div class="card-text">
-                                            <h4 class="card-title text-center">Previosly Annotated Image</h4>
+                                            <h4 class="card-title">Caption</h4>
                                         </div>
                                     </div>
                                     <div class="card-body">
-                                        <p class="card-text">Some annotations<p>
-                                    </div>
-                                    <img class="card-img-top" src="DIDEC/images/133.jpg">
-                                    <div class="card-body">
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <p class="card-text" id="captionCardText">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-6">
-                                <div class="card">
+                            <div class="col-md-12" id="captionWithImage">
+                                <div class="card" style="width: 60%; margin-left:auto; margin-right:auto;">
                                     <div class="card-header card-header-text card-header-primary">
                                         <div class="card-text">
-                                            <h4 class="card-title text-center">Current Image</h4>
+                                            <h4 class="card-title">Image with caption</h4>
                                         </div>
                                     </div>
                                     <div class="card-body">
+                                        <p class="card-text" id="captionImageCardText"></p>
+                                    </div>
+                                    <div class="card-body">
+                                        <img class='card-img-top' id='Image' src=''>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="card" style="width: 60%; margin-left:auto; margin-right:auto;">
+                                    <div class="card-header card-header-text card-header-primary">
+                                        <div class="card-text">
+                                            <h4 class="card-title">Annotations</h4>
+                                        </div>
+                                    </div>
+
+                                    <div class="card-body" id="annotation">
                                         <form action="annotator.php" method="GET">
                                             <div class="form-row">
                                                 <div class="form-group col-md-6">
@@ -76,13 +130,9 @@ session_start()
                                                     <input type="text" class="form-control" id="inputAnnotate" name="inputAnnotate" placeholder="Annotations">
                                                 </div>
                                                 <div class="form-group col-md-6">
-                                                    <button type="submit" class="btn btn-primary" style="margin-top: 9%; margin-left:20%;">Submit</button>
+                                                    <button type="button" class="btn btn-primary" style="margin-top: 9%; margin-left:20%;" onclick="loadCaptionsAndImages()">Submit</button>
                                                 </div>
                                         </form>
-                                    </div>
-                                    <img class="card-img-top" src="DIDEC/images/133.jpg">
-                                    <div class="card-body">
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                     </div>
                                 </div>
                             </div>
